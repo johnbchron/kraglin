af41f56077272adb8ef3d7d4f320e4879a9703b7
John Lewis <github@jlewis.sh>
2024-04-02 16:03:48 +0200

    feat: add error type and `Backend` trait

--- src/main.rs
+++ src/main.rs
@@ -112,6 +112,24 @@ pub enum Command {
   RightPop { key: SmolStr },
 }
 
+/// The conglomerate error type for all [`kraglin`](crate) commands.
+#[derive(Debug, Clone, thiserror::Error)]
+pub enum KraglinError {}
+
+/// A convenience type alias for a oneshot receiver with a [`kraglin`](crate)
+/// result.
+type ResultReceiver = oneshot::Receiver<Result<Value, KraglinError>>;
+
+/// The generalized backend trait. All storage/execution backends implement
+/// this.
+pub trait Backend {
+  fn execute(
+    &self,
+    command: Command,
+    result_channel: ResultReceiver,
+  ) -> impl Future<Output = ()> + Send;
+}
+
 #[tokio::main]
 async fn main() {
   println!("Hello, world!");
